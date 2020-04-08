775d2f8b2bba7c434e38ee1344db862a2f33d34c
Charlie Pham <phamc5@wit.edu>
2020-04-08 18:44:08 -0400

    Initial commit

---
+++ src/main/java/com/demo/FizzBuzz.java
@@ -0,0 +1,36 @@
+package com.demo;
+
+public class FizzBuzz 
+{
+    String processNumber(int num)
+	{
+		if(num%15 == 0 )
+		{
+			return "FizzBuzz";
+		}
+		if(num%3 == 0 )
+		{
+			return "Fizz";
+		}
+		if (num%5 == 0)
+		{
+			return "Buzz";	
+		}
+		return String.valueOf(num);
+	}
+	
+	String processNumbers (int[] numList)
+	{
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < numList.length; i++)
+		{
+			result.append(processNumber(numList[i]));
+			if (i < numList.length-1)
+			{
+				result.append(", ");
+			}
+		}
+		return result.toString();
+	}
+
+}
